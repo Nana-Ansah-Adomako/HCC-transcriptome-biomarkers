eaf4349ebe31e95b30ddd2576c5f2d124fd2c121
Nana-Ansah-Adomako <nanaadomakoansah@gmail.com>
2025-11-25 15:16:46 +0000

    finding_intersections_in_results

---
+++ scripts/04_integrate_and_find_common_DEGs.R
@@ -0,0 +1,10 @@
+#To find common genes across all highly DEG files
+file1 <- read.csv("/home/nana-ansah-adomako/HCC/sig_genes1.csv")
+file1 <- file1$x
+file2 <- conversion$hgnc_symbol
+file3 <- rownames(significant_degs)
+
+
+gene_list <- intersect(intersect(file1,file2), file3)
+
+write.csv(gene_list, "gene_list.csv")
