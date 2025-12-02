6747a0dfa1be26a12e54e38b5b9467d181a226d5
Nana-Ansah-Adomako <nanaadomakoansah@gmail.com>
2025-12-02 15:29:06 +0000

    added two libraries to setup file

--- scripts/00_setup_environment.R
+++ scripts/00_setup_environment.R
@@ -6,7 +6,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 libs <- c("GEOquery", "DESeq2", "limma", "org.Hs.eg.db", "biomaRt",
-          "EnhancedVolcano", "ggplot2", "dplyr", "tibble", "hgu133plus2.db")
+          "EnhancedVolcano", "ggplot2", "dplyr", "tibble", "hgu133plus2.db", "tidyr", "patchwork")
 
 BiocManager::install(libs, ask = FALSE, update = TRUE)
 lapply(libs, library, character.only = TRUE)
